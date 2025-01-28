b39f23e754330bbd2037d9c2b621441902569f98
David-Portugal-Ferreira <davidportugal44@gmail.com>
2025-01-28 17:39:34 +0000

    Change giphy img from assets to public

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
       <div className="footer">
         <img
-          src='./src/assets/giphy-img.gif'
-          alt=""
+          src='/giphy-img.gif'
+          alt="giphy logo"
         />
       </div>
     );
